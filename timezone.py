908c12d337d3f9839b0c09f8d3df3db0cddd2515
Arash Homayounfar <93074385+odoo-app-dev@users.noreply.github.com>
2022-12-20 10:39:44 +0330

    Update timezone.py

--- timezone.py
+++ timezone.py
@@ -2,7 +2,10 @@ from odoo import models, fields, api, _
 from datetime import datetime, timedelta
 import pytz
 
-  tz = self.env['ir.config_parameter'].sudo().get_param('sd_food.tz')
-  time_z = pytz.timezone(tz or 'UTC')
-  datetime.today().date(time_z)
-  datetime.now(time_z).hour > 14
+  tz = self.env['ir.config_parameter'].sudo().get_param('sd_food.tz')  # Asia/Tehran
+  
+  time_z = pytz.timezone(tz or 'UTC')     # Asia/Tehran
+  datetime.now()                          # 2022-12-20 07:02:17.357002
+  datetime.now(time_z)                    # 2022-12-20 10:32:17.357020+03:30
+  datetime.now(time_z).date()             # 2022-12-20
+
