0085680b808a6c0a912aaf444eb15a63b3136877
Arash Homayounfar <93074385+odoo-app-dev@users.noreply.github.com>
2022-12-20 15:08:34 +0330

    Update notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,13 +1,15 @@
 odoo.define('sd_food.refresh_div', require => {
     'use strict';
     var FormController = require('web.FormController');
-    var core = require('web.core');
-    var _t = core._t;
+    var core = require('web.core');  // needed
+    var _t = core._t; //needed
 
     var ExtendFormController = FormController.include({
     saveRecord: function() {
         if (this.modelName == 'sd_food.groups')
-            this.displayNotification({ message: _t('This form is saved'), type: 'success', sticky: true,});   // success, danger, warning
+            this.displayNotification({ message: _t('This form is saved'), type: 'success', sticky: true,});   // needed
+            // type:[success, danger, warning]
+            // sticky: [false]-> disapear after 5 seconds [true]-> needed to be closed manualy.
         var res = this._super.apply(this, arguments);
         return res
         }
