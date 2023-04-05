b3b801a523e02dc8e5d3e4ca8746620cd0f693f4
Arash Homayounfar <93074385+odoo-app-dev@users.noreply.github.com>
2023-04-05 12:56:23 +0330

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -1,6 +1,10 @@
-  
+#   addons/maintenance/mantenance.py:L200
   _sql_constraints = [
         ('serial_no', 'unique(serial_no)', "Another asset already exists with this serial number!"),
     ]
-                   
-                   
+           
+#     addons/base/models/res_partner.py:L440
+    @api.constrains('barcode')
+    def _check_barcode_unicity(self):
+        if self.barcode and self.env['res.partner'].search_count([('barcode', '=', self.barcode)]) > 1:
+            raise ValidationError('An other user already has this barcode')                
